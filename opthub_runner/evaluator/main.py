88087cf5e9082242094c064671a457b16f2b81ed
kkkyosyo <kimura.k.az@m.titech.ac.jp>
2024-03-25 00:49:29 +0900

    Update evaluator/main.py and scorer/main.py

--- opthub_runner/evaluator/main.py
+++ opthub_runner/evaluator/main.py
@@ -1,38 +1,65 @@
-import json
-import logging
-import signal
 from datetime import datetime
+import signal
+import logging
 from traceback import format_exc
+import json
 
-from opthub_runner.models.evaluation import save_failed_evaluation, save_success_evaluation
+from opthub_runner.lib.runner_sqs import EvaluatorSQS
+from opthub_runner.lib.dynamodb import DynamoDB
+from opthub_runner.lib.docker_executor import execute_in_docker
 from opthub_runner.models.match import fetch_match_problem_by_id
 from opthub_runner.models.solution import fetch_solution_by_primary_key
-from opthub_runner.lib.docker_executor import execute_in_docker
-from opthub_runner.lib.dynamodb import DynamoDB
-from opthub_runner.lib.keys import ACCESS_KEY_ID, QUEUE_NAME, REGION_NAME, SECRET_ACCESS_KEY, TABLE_NAME
-from opthub_runner.lib.runner_sqs import EvaluatorSQS
+from opthub_runner.models.evaluation import save_success_evaluation, save_failed_evaluation
+from opthub_runner.lib.keys import QUEUE_NAME, ACCESS_KEY_ID, SECRET_ACCESS_KEY, REGION_NAME, TABLE_NAME
 
-def main():
+
+LOGGER = logging.getLogger(__name__)
+
+
+def evaluate(ctx, **kwargs):
+    """
+    評価プロセスのコントローラーを行う関数．
+
+    """
+
     # Amazon SQSとのやり取り用
-    sqs = RunnerSQS("tmp_name")
-    # sqs = RunnerSQS(kwargs["queue_name"])
+    sqs = EvaluatorSQS(QUEUE_NAME, 2.0)
+    #sqs = RunnerSQS(kwargs["queue_name"], kwargs["interval"])
 
     # Dynamo DBとのやり取り用
-    dynamodb = DynamoDB("http://localhost:8000", "localhost", "aaa", "aaa", "opthub-dynamodb-participant-trials-dev")
+    dynamodb = DynamoDB(REGION_NAME,
+                        ACCESS_KEY_ID,
+                        SECRET_ACCESS_KEY,
+                        TABLE_NAME)
     # dynamodb = DynamoDB(kwargs["endpoint_url"],
     #                     kwargs["region_name"],
     #                     kwargs["aws_access_key_id"],
-    #                      kwargs["aws_secret_access_key_id"],
+    #                     kwargs["aws_secret_access_key_id"],
     #                     kwargs["table_name"])
-    sqs = EvaluatorSQS(QUEUE_NAME, 2.0)
-    # sqs = RunnerSQS(kwargs["queue_  REGION_NAME, ACCESS_KEY_ID, SECRET_ACCESS_KEY, TABLE_NAME)
+    
+    n_evaluation = 0
+
+    while True:
+        n_evaluation += 1
+        LOGGER.info("==================== Evaluation: %d ====================", n_evaluation)
+
+        try:
+            # Partition KeyのためのParticipantID，Trialを取得
+            LOGGER.info("Find Solution to evaluate...")
+            partition_key_data = sqs.get_partition_key_from_queue()
+            LOGGER.info("...Found")
+
+            # MatchIDからProblemEnvironmentsとProblemDockerImageを取得
+            LOGGER.info("Fetch problem data from DB...")
+            problem_data = fetch_match_problem_by_id(kwargs["match_id"])
             LOGGER.info("...Fetched")
 
             # Partition Keyを使ってDynamo DBからSolutionを取得
             LOGGER.info("Fetch Solution from DB...")
-            solution = fetch_solution_by_primary_key(
-            args["match_id"], partition_key_data["ParticipantID"], partition_key_data["Trial"], dynamodb
-            )
+            solution = fetch_solution_by_primary_key(kwargs["match_id"],
+                                                    partition_key_data["ParticipantID"],
+                                                    partition_key_data["Trial"],
+                                                    dynamodb)
             LOGGER.info("...Fetched")
 
         except KeyboardInterrupt:
@@ -41,7 +68,11 @@ def main():
             LOGGER.error("Keyboard Interrupt")
             signal.signal(signal.SIGTERM, signal.SIG_DFL)
             signal.signal(signal.SIGINT, signal.SIG_DFL)
-            partition_key_data = sqs.get_partition_key_from_queue()
+            ctx.exit(0)
+
+        except Exception:
+            LOGGER.error("Exception")
+            LOGGER.error(format_exc())
             continue
 
         try:
@@ -51,14 +82,13 @@ def main():
             started_at = started_at.isoformat()
             LOGGER.info(f"Started at : {started_at}")
 
-            # Docker Imageを使ってObjective，Constraint，In
-                foを取得   dynamodb
-
-                kwargs["command"],
-                kwargs["timeout"],
-                kwargs["rm"],
-                json.dumps(solution["Variable"]) + "\n",
-            )
+            # Docker Imageを使ってObjective，Constraint，Infoを取得
+            evaluation_result = execute_in_docker(problem_data["ProblemDockerImage"],
+                                                  problem_data["ProblemEnvironments"],
+                                                  kwargs["command"],
+                                                  kwargs["timeout"],
+                                                  kwargs["rm"],
+                                                  json.dumps(solution["Variable"]) + "\n")
 
             if "error" in evaluation_result:
                 raise Exception("Error occurred while evaluating solution:\n" + evaluation_result["error"])
@@ -77,57 +107,50 @@ def main():
 
             LOGGER.info("Save Evaluation...")
             # 成功試行をDynamo DBに保存
-            save_success_evaluation(
-
-],
-antID"],
-],
-t"],
-,
-
-                finished_at,
-                evaluation_result["objective"],
-                evaluation_result["constraint"],
-                evaluation_result["info"],
-                evaluation_result["feasible"],
-                dynamodb,
-            )
+            save_success_evaluation(solution["MatchID"],
+                                    solution["ParticipantID"],
+                                    solution["TrialNo"],
+                                    solution["CreatedAt"],
+                                    started_at,
+                                    finished_at,
+                                    evaluation_result["objective"],
+                                    evaluation_result["constraint"],
+                                    evaluation_result["info"],
+                                    evaluation_result["feasible"],
+                                    dynamodb)
             LOGGER.info("...Saved")
 
+            sqs.delete_partition_key_from_queue()
+            
         except KeyboardInterrupt:
             signal.signal(signal.SIGTERM, signal.SIG_IGN)
             signal.signal(signal.SIGINT, signal.SIG_IGN)
             LOGGER.error("Keyboard Interrupt")
             finished_at = datetime.now()
             finished_at = finished_at.isoformat()
-            save_failed_evaluation(
-                solution["MatchID"],
-                solution["ParticipantID"],
-                solution["TrialNo"],
-
-tion["CreatedAt"],
-           start
-finished_at,
-    format_exc()
-         dynamod
-
-gnal.signal(signTERM, signal.SIG_DFL)
-   signal.signalINT, signal.SIG_DFL)
-            ctx.
-        except Edynamodb,
-xception:
+            save_failed_evaluation(solution["MatchID"],
+                                   solution["ParticipantID"],
+                                   solution["TrialNo"],
+                                   solution["CreatedAt"],
+                                   started_at,
+                                   finished_at,
+                                   format_exc(),
+                                   dynamodb)
+            signal.signal(signal.SIGTERM, signal.SIG_DFL)
+            signal.signal(signal.SIGINT, signal.SIG_DFL)
+            ctx.exit(0)
+        except Exception:
             finished_at = datetime.now()
             finished_at = finished_at.isoformat()
-            save_failed_evaluation(
-    solution["MatchID"],
-                solution["ParticipantID"],
-            sqs.delete_partition_key_from_queue()
-
-                dynamodb,
-            )
+            save_failed_evaluation(solution["MatchID"],
+                                   solution["ParticipantID"],
+                                   solution["TrialNo"],
+                                   solution["CreatedAt"],
+                                   started_at,
+                                   finished_at,
+                                   format_exc(),
+                                   dynamodb)
             LOGGER.error(format_exc())
+
+            sqs.delete_partition_key_from_queue()
             continue
-
-dynamodb,
-
-                dynamodb,
--- opthub_runner/scorer/main.py
+++ opthub_runner/scorer/main.py
@@ -1,19 +1,20 @@
-import json
-import logging
-import signal
 from datetime import datetime
+import signal
+import logging
 from traceback import format_exc
+import json
 
-from opthub_runner.lib.cache import Cache
-from opthub_runner.lib.docker_executor import execute_in_docker
-from opthub_runner.lib.dynamodb import DynamoDB
-from opthub_runner.lib.keys import ACCESS_KEY_ID, QUEUE_NAME, REGION_NAME, SECRET_ACCESS_KEY, TABLE_NAME
 from opthub_runner.lib.runner_sqs import ScorerSQS
-from opthub_runner.lib.scorer_history import make_history, write_to_cache
-from opthub_runner.lib.zfill import zfill
-from opthub_runner.models.evaluation import fetch_evaluation_by_primary_key
+from opthub_runner.lib.dynamodb import DynamoDB
+from opthub_runner.lib.docker_executor import execute_in_docker
 from opthub_runner.models.match import fetch_match_indicator_by_id
-from opthub_runner.models.score import save_failed_score, save_success_score
+from opthub_runner.models.evaluation import fetch_success_evaluation_by_primary_key
+from opthub_runner.models.score import save_success_score, save_failed_score
+from opthub_runner.lib.scorer_history import make_history, write_to_cache
+from opthub_runner.lib.cache import Cache
+from opthub_runner.lib.zfill import zfill
+from opthub_runner.lib.keys import QUEUE_NAME, ACCESS_KEY_ID, SECRET_ACCESS_KEY, REGION_NAME, TABLE_NAME
+
 
 LOGGER = logging.getLogger(__name__)
 
@@ -28,7 +29,10 @@ def calculate_score(ctx, **kwargs):
     sqs = ScorerSQS(QUEUE_NAME, 2.0)
 
     # Dynamo DBとのやり取り用
-    dynamodb = DynamoDB(REGION_NAME, ACCESS_KEY_ID, SECRET_ACCESS_KEY, TABLE_NAME)
+    dynamodb = DynamoDB(REGION_NAME,
+                        ACCESS_KEY_ID,
+                        SECRET_ACCESS_KEY,
+                        TABLE_NAME)
     # dynamodb = DynamoDB(kwargs["endpoint_url"],
     #                     kwargs["region_name"],
     #                     kwargs["aws_access_key_id"],
@@ -37,7 +41,7 @@ def calculate_score(ctx, **kwargs):
 
     # cacheファイルの管理用
     cache = Cache()
-
+    
     n_score = 0
 
     while True:
@@ -57,26 +61,24 @@ def calculate_score(ctx, **kwargs):
 
             # Partition Keyを使ってDynamo DBからEvaluationを取得
             LOGGER.info("Fetch Evaluation from DB...")
-            evaluation = fetch_evaluation_by_primary_key(
-                kwargs["match_id"], partition_key_data["ParticipantID"], partition_key_data["Trial"], dynamodb
-            )
+            evaluation = fetch_success_evaluation_by_primary_key(kwargs["match_id"],
+                                                         partition_key_data["ParticipantID"],
+                                                         partition_key_data["Trial"],
+                                                         dynamodb)
             LOGGER.info("...Fetched")
 
             # currentとhistoryを作成（Dockerの入力）
             LOGGER.info("Make history...")
-            current = {
-                "objective": evaluation["Objective"],
-                "constraint": evaluation["Constraint"],
-                "info": evaluation["Info"],
-            }
-            history = make_history(
-                evaluation["MatchID"],
-                evaluation["ParticipantID"],
-                zfill(int(evaluation["TrialNo"]) - 1, len(evaluation["TrialNo"])),
-                cache,
-                dynamodb,
-            )
+            current = {"objective": evaluation["Objective"],
+                       "constraint": evaluation["Constraint"],
+                       "info": evaluation["Info"]}
+            history = make_history(evaluation["MatchID"],
+                                   evaluation["ParticipantID"],
+                                   zfill(int(evaluation["TrialNo"]) - 1, len(evaluation["TrialNo"])),
+                                   cache,
+                                   dynamodb)
             LOGGER.info("...Made")
+
 
         except KeyboardInterrupt:
             signal.signal(signal.SIGTERM, signal.SIG_IGN)
@@ -99,85 +101,76 @@ def calculate_score(ctx, **kwargs):
             LOGGER.info(f"Started at : {started_at}")
 
             # Docker Imageを使ってScoreを取得
-            score_result = execute_in_docker(
-                indicator_data["IndicatorDockerImage"],
-                indicator_data["IndicatorEnvironments"],
-                kwargs["command"],
-                kwargs["timeout"],
-                kwargs["rm"],
-                json.dumps(current) + "\n",
-                json.dumps(history) + "\n",
-            )
-
+            score_result = execute_in_docker(indicator_data["IndicatorDockerImage"],
+                                             indicator_data["IndicatorEnvironments"],
+                                             kwargs["command"],
+                                             kwargs["timeout"],
+                                             kwargs["rm"],
+                                             json.dumps(current) + "\n",
+                                             json.dumps(history) + "\n")
+            
             if "error" in score_result:
                 raise Exception("Error occurred while calculating score:\n" + score_result["error"])
-
+            
             LOGGER.info("...Calculated")
             # スコア計算終了時刻の記録
             finished_at = datetime.now()
             finished_at = finished_at.isoformat()
             LOGGER.info(f"Finished at : {finished_at}")
 
+
             LOGGER.info("Save Score...")
             # cacheにスコアを保存
-            write_to_cache(
-                evaluation["MatchID"],
-                evaluation["ParticipantID"],
-                evaluation["TrialNo"],
-                current["objective"],
-                current["constraint"],
-                current["info"],
-                score_result["score"],
-                cache,
-            )
-
+            write_to_cache(evaluation["MatchID"],
+                           evaluation["ParticipantID"],
+                           evaluation["TrialNo"],
+                           current["objective"],
+                           current["constraint"],
+                           current["info"],
+                           score_result["score"],
+                           cache)
+            
             # 成功試行をDynamo DBに保存
-            save_success_score(
-                evaluation["MatchID"],
-                evaluation["ParticipantID"],
-                evaluation["TrialNo"],
-                evaluation["CreatedAt"],
-                started_at,
-                finished_at,
-                score_result["score"],
-                dynamodb,
-            )
+            save_success_score(evaluation["MatchID"],
+                               evaluation["ParticipantID"],
+                               evaluation["TrialNo"],
+                               evaluation["CreatedAt"],
+                               started_at,
+                               finished_at,
+                               score_result["score"],
+                               dynamodb)
             LOGGER.info("...Saved")
 
             sqs.delete_partition_key_from_queue()
-
+            
         except KeyboardInterrupt:
             signal.signal(signal.SIGTERM, signal.SIG_IGN)
             signal.signal(signal.SIGINT, signal.SIG_IGN)
             LOGGER.error("Keyboard Interrupt")
             finished_at = datetime.now()
             finished_at = finished_at.isoformat()
-            save_failed_score(
-                evaluation["MatchID"],
-                evaluation["ParticipantID"],
-                evaluation["TrialNo"],
-                evaluation["CreatedAt"],
-                started_at,
-                finished_at,
-                format_exc(),
-                dynamodb,
-            )
+            save_failed_score(evaluation["MatchID"],
+                              evaluation["ParticipantID"],
+                              evaluation["TrialNo"],
+                              evaluation["CreatedAt"],
+                              started_at,
+                              finished_at,
+                              format_exc(),
+                              dynamodb)
             signal.signal(signal.SIGTERM, signal.SIG_DFL)
             signal.signal(signal.SIGINT, signal.SIG_DFL)
             ctx.exit(0)
         except Exception:
             finished_at = datetime.now()
             finished_at = finished_at.isoformat()
-            save_failed_score(
-                evaluation["MatchID"],
-                evaluation["ParticipantID"],
-                evaluation["TrialNo"],
-                evaluation["CreatedAt"],
-                started_at,
-                finished_at,
-                format_exc(),
-                dynamodb,
-            )
+            save_failed_score(evaluation["MatchID"],
+                              evaluation["ParticipantID"],
+                              evaluation["TrialNo"],
+                              evaluation["CreatedAt"],
+                              started_at,
+                              finished_at,
+                              format_exc(),
+                              dynamodb)
             LOGGER.error(format_exc())
 
             sqs.delete_partition_key_from_queue()
